11e33af3691e5d99ede4cc433c8116e24e076d30
Reksa Syahputra <reksa.prayoga1012@gmail.com>
2024-01-17 20:47:20 +0700

    add management order page

---
+++ lib/pages/managementorder_page.dart
@@ -0,0 +1,205 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:bottom_navigation_view/bottom_navigation_view.dart';
+import 'dashboard_page.dart';
+import 'search_page.dart';
+import 'favorite_page.dart';
+import 'profile_page.dart';
+import 'exchange_page.dart';
+import '../theme/app_colors.dart';
+
+class ManagementOrderPage extends StatefulWidget {
+  const ManagementOrderPage({Key? key}) : super(key: key);
+
+  @override
+  State<ManagementOrderPage> createState() => _ManagementOrderPageState();
+}
+
+class _ManagementOrderPageState extends State<ManagementOrderPage>
+    with SingleTickerProviderStateMixin {
+  late final BottomNavigationController _controller;
+  DateTime? currentBackPressTime;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = BottomNavigationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    late List<Map<String, String>> items = [
+      {
+        'name': 'Korean Corn Sausage',
+        'price': '100 Koin',
+        'image': 'assets/makanan.png',
+        'id': '#16234'
+      },
+      {
+        'name': 'Burger',
+        'price': '400 Koin',
+        'image': 'assets/makanan.png',
+        'id': '#16234'
+      }
+    ];
+
+    return WillPopScope(
+        onWillPop: () async {
+          DateTime now = DateTime.now();
+          if (currentBackPressTime == null ||
+              now.difference(currentBackPressTime!) >
+                  const Duration(seconds: 2)) {
+            currentBackPressTime = now;
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('Press back again to exit the app'),
+              ),
+            );
+            return false;
+          }
+          return true;
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            title: Text('Management Pesanan'),
+            centerTitle: true,
+          ),
+          body: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                // mapping the items list here
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: items.length,
+                    itemBuilder: (context, index) {
+                      return Column(
+                        children: [
+                          Row(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: Image.asset(
+                                  'assets/makanan.png',
+                                  width: 80.0,
+                                  height: 80.0,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              SizedBox(width: 16),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      items[index]['name']!,
+                                      style: TextStyle(
+                                        fontSize: 16.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    SizedBox(height: 10),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Text(
+                                              items[index]['price']!,
+                                              style: TextStyle(
+                                                fontSize: 16.0,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Text('#16234')
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              SizedBox(
+                                height: 40,
+                                width: 100,
+                                child: Card(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8.0),
+                                  ),
+                                  color: AppColors.primary,
+                                  child: const Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        SizedBox(width: 10),
+                                        Text(
+                                          'Terima',
+                                          style: TextStyle(
+                                              fontSize: 14,
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        SizedBox(width: 10),
+                                      ]),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 40,
+                                width: 100,
+                                child: Card(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8.0),
+                                  ),
+                                  color: AppColors.secondary,
+                                  child: const Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        SizedBox(width: 10),
+                                        Text(
+                                          'Reject',
+                                          style: TextStyle(
+                                              fontSize: 14,
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        SizedBox(width: 10),
+                                      ]),
+                                ),
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+}
