acca0a07531509ddde848061b2227c1226659749
divaariani <divaarifinb@gmail.com>
2024-02-25 17:09:05 +0700

    implement google cloud runner

--- lib/services/api_config.dart
+++ lib/services/api_config.dart
@@ -1,5 +1,6 @@
 class AppConfig {
-  //static const String apiBaseUrl = 'https://echobyte-api.000webhostapp.com';
+  // static const String apiBaseUrl = 'https://echobyte-api.000webhostapp.com';
   static const String apiBaseUrl = 'http://203.194.112.77';
-  //static const String apiBaseUrl = 'https://test-gcp-5ss4dsr4la-uc.a.run.app';
+  // static const String apiBaseUrl = 'https://test-gcp-5ss4dsr4la-uc.a.run.app';
+  // static const String apiBaseUrl = 'https://trace2treat-backend-5ss4dsr4la-et.a.run.app';
 }
